4135bd7a9ed626369841302e7955cb48ad4397f9
club3 <club3@yeah.net>
2016-02-26 23:45:54 +0800

    add xmlHelper class

---
+++ src/git_demo/xmlHelper.java
@@ -0,0 +1,13 @@
+package git_demo;
+
+public class xmlHelper {
+	public String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
