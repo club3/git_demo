087bcec7a87fe82717e20ede26f571d43c740196
club3 <club3@yeah.net>
2016-02-26 22:46:04 +0800

    dfdfd

---
+++ src/git_demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package git_demo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(100);//这样是直接输出变量
+		System.out.println("aaa");
+	}
+
+}
